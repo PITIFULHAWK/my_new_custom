a4a2014eb2d84521e118ce36a0298d4cd2a5a410
PITIFULHAWK <xxxxyyyy1920@gmail.com>
2024-10-07 01:11:51 +0530

    added some code in options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,7 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.scrolloff = 7
+o.cursorline = true
+o.cursorlineopt = "both" -- to enable cursorline!
